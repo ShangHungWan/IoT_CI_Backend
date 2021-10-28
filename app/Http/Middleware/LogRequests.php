dce488440181d6c691b76b3087456652b7a8d683
Sun <a24230928@gmail.com>
2021-10-28 20:27:18 +0800

    feat: add log requests middleware

---
+++ app/Http/Middleware/LogRequests.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class LogRequests
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        Log::info('request', ['request' => $request]);
+        $response = $next($request);
+        Log::info('response', ['request' => $response]);
+        return $response;
+    }
+}
